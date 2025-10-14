78e5c830bcbd7ca85513ae80191764f1ae384fc5
umidsamadov0927 <umidsamadov0927@gmail.com>
2025-10-14 13:59:54 +0500

    add

---
+++ src/pages/document.jsx
@@ -0,0 +1,9 @@
+export default function Document(){
+    return (
+        <>
+            <div>
+                <h1 className="text-black text-[24px] text-center mt-[20%] font-[600]">Document page</h1>
+            </div>
+        </>
+    )
+}
--- src/router/route.jsx
+++ src/router/route.jsx
@@ -8,13 +8,16 @@ import Error from "../pages/error.jsx";
 import Contact from "../pages/Contact.jsx";
 import Api from "../pages/api.jsx";
 import ProductById from "../pages/productById.jsx";
+import Document from "../pages/document.jsx";
 
 export default function Routers() {
     return (
         <>
             <Header/>
             <Routes>
-                <Route path="/" element={<Home/>}/>
+                <Route path="/" element={<Home/>}>
+                    <Route path='/document' element={<Document/>}></Route>
+                </Route>
                 <Route path="/about" element={<About/>}/>
                 <Route path="/contact" element={<Contact/>}/>
                 <Route path="/tutorial" element={<Tutorial/>}/>
